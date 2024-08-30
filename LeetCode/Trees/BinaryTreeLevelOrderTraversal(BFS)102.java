4f2f6d8eba67e55fe2f7861acd14a4573a81abb2
Camilo Potin <133172371+CamiloPotin@users.noreply.github.com>
2024-08-30 14:24:48 -0300

    Create BinaryTreeLevelOrderTraversal(BFS)102.java

---
+++ LeetCode/Trees/BinaryTreeLevelOrderTraversal(BFS)102.java
@@ -0,0 +1,61 @@
+/*
+102. Binary Tree Level Order Traversal
+
+Problema:
+Se quiere recorrer un arbol por niveles y devolver un arreglo que cada elemento sea un arreglo con los valores
+de cada nivel
+
+Solucion:
+Se recorre usando BFS y guarda por nivel.
+
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+
+
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) return result;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            List<Integer> level = new ArrayList<>();
+            int n = queue.size();
+
+            for (int i = 0; i < n; i++) {
+                TreeNode node = queue.poll();
+                level.add(node.val);
+
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
+            }
+
+            result.add(level);
+        }
+
+        return result;
+
+
+
+
+
+    }
+}
