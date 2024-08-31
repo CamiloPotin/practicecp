7497354ec5990b90cd5440586411ae20df03f328
Camilo Potin <133172371+CamiloPotin@users.noreply.github.com>
2024-08-31 16:21:52 -0300

    Create ValidateBinarySeachTree98.java

---
+++ LeetCode/Trees/ValidateBinarySeachTree98.java
@@ -0,0 +1,48 @@
+/*
+98. Validate Binary Search Tree
+
+Problema:
+Se quiere saber si un BST es valido
+
+Solucion:
+Se usa un metodo en el que se manda a cada hijo el valor del nodo como min o max segun corresponda
+y se compara
+
+
+
+
+*/
+
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+    
+    private boolean isValidBST(TreeNode node, long min, long max) {
+        if (node == null) {
+            return true;
+        }
+        if (node.val <= min || node.val >= max) {
+            return false;
+        }
+        return isValidBST(node.left, min, node.val) && isValidBST(node.right, node.val, max);
+    }
+}
