7d22ff43d475fa9328cd017e41cbdb626200c82b
Camilo Potin <133172371+CamiloPotin@users.noreply.github.com>
2024-11-13 12:00:24 -0300

    Create CountTheNumberOfFairPairs2563.java

---
+++ LeetCode/TwoPointers/CountTheNumberOfFairPairs2563.java
@@ -0,0 +1,29 @@
+/*
+2563. Count the Number of Fair Pairs
+*/
+
+class Solution {
+    public long countFairPairs(int[] nums, int lower, int upper) {
+              Arrays.sort(nums);
+        return lower_bound(nums, upper + 1) - lower_bound(nums, lower);
+    }
+
+    private long lower_bound(int[] nums, int value) {
+        int left = 0, right = nums.length - 1;
+        long result = 0;
+        while (left < right) {
+            int sum = nums[left] + nums[right];
+            // If sum is less than value, add the size of window to result and move to the
+            // next index.
+            if (sum < value) {
+                result += (right - left);
+                left++;
+            } else {
+                // Otherwise, shift the right pointer backwards, until we get a valid window.
+                right--;
+            }
+        }
+
+        return result;
+    }
+}
